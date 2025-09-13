af4c7d967d10ca97acf2c5ebbfbaf276d4fa2130
Priyanka <2021wc86694@wilp.bits-pilani.ac.in>
2025-09-13 18:12:44 +0530

    chnages

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Quiz App!</h1>
+      <p>This is your React frontend running inside Docker 🚀</p>
+    </div>
+  );
+}
+
+export default App;
